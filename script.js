f2ea4fe141c1a957b47fec71e2af0d5726884062
Boasbabs <babalolasimeon@gmail.com>
2016-05-26 09:16:10 +0530

    data binding

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var app = angular.module("Todo", []);
 
 app.controller("TodoCtrl", function ($scope) {
-	$scope.message = "Angular is pretty cool.";
+	$scope.todos = [
+		"Learn Sketch",
+		"Look at Dribble and feel inferior",
+		"Actually learn how to use the Pen tool"
+	];
 });
